d2d38eea920b7dd3f47dfe27cb47bafbf844d0d2
ayush7166 <ayushbansal7166@gmail.com>
2026-03-31 14:55:14 +0530

    Resolved merge conflicts

--- models/userschema.js
+++ models/userschema.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose");
-<<<<<<< HEAD
+
 const passportlocalmongoose=require("passport-local-mongoose").default;
-=======
+
 const passportlocalmongoose=require("passport-local-mongoose");
->>>>>>> 4ee5ea57237774efbc251b794e72f99ae6f12045
+
 
 
 const userschema=new mongoose.Schema({
